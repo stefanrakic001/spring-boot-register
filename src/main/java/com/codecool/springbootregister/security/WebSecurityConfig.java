333b2270376862313a4755df02b22569b0a716f5
pappgeri096 <pappgergo096@gmail.com>
2019-04-30 09:42:32 +0200

    removed unused files and routes

--- src/main/java/com/codecool/springbootregister/security/WebSecurityConfig.java
+++ src/main/java/com/codecool/springbootregister/security/WebSecurityConfig.java
@@ -57,9 +57,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and().csrf().disable().
                 authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
-                .antMatchers("/api/user/**").permitAll()
-                .antMatchers("/api/lodgings/**").permitAll()
-                .antMatchers("/api/todos/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
