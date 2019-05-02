42f42c5a93dae17092f6d77ac7e55bde218e427a
stefanrakic001 <stefan.rakic001@gmail.com>
2019-05-02 15:32:24 +0200

    Sample Employee is created in Init

--- src/main/java/com/codecool/springbootregister/config/Init.java
+++ src/main/java/com/codecool/springbootregister/config/Init.java
@@ -1,28 +1,40 @@
 package com.codecool.springbootregister.config;
 
+import com.codecool.springbootregister.model.Car;
+import com.codecool.springbootregister.model.Employee;
 import com.codecool.springbootregister.model.Role;
 import com.codecool.springbootregister.model.User;
+import com.codecool.springbootregister.repository.CarRepository;
+import com.codecool.springbootregister.repository.EmployeeRepository;
 import com.codecool.springbootregister.repository.RoleRepository;
 import com.codecool.springbootregister.repository.UserRepository;
+import com.codecool.springbootregister.util.AvailabilityType;
 import com.codecool.springbootregister.util.RoleName;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
 @Component
 public class Init implements CommandLineRunner {
 
+    private CarRepository carRepository;
+
     private PasswordEncoder encoder;
+
+    private EmployeeRepository employeeRepository;
 
     private UserRepository userRepository;
 
     private RoleRepository roleRepository;
 
-    public Init(PasswordEncoder encoder, UserRepository userRepository, RoleRepository roleRepository) {
+    public Init(CarRepository carRepository, PasswordEncoder encoder, EmployeeRepository employeeRepository, UserRepository userRepository, RoleRepository roleRepository) {
+        this.carRepository = carRepository;
         this.encoder = encoder;
+        this.employeeRepository = employeeRepository;
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
     }
@@ -30,6 +42,24 @@ public class Init implements CommandLineRunner {
     @Override
     public void run(String... args) {
         Set<Role> roles = new HashSet<>();
+        Car myCar = Car.builder()
+                .carType("BMW")
+                .licencePlate("HTM-222").build();
+
+        carRepository.save(myCar);
+
+
+        Employee employee = Employee.builder()
+                .address("Ló utca 2")
+                .availability(AvailabilityType.AVAILABLE)
+                .car(myCar)
+                .hireDate(LocalDate.now())
+                .construction("Béla utca 23")
+                .name("István Péter").build();
+
+        employeeRepository.save(employee);
+
+
         User user = new User("admin", encoder.encode("12345678"), roles);
 
         roleRepository.save(new Role(RoleName.ROLE_USER));
