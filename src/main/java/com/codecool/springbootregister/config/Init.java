424d66f138848d102a75e1396d99ad7c5ff61274
stefanrakic001 <stefan.rakic001@gmail.com>
2019-05-09 12:52:25 +0200

    sample data is added to Init

--- src/main/java/com/codecool/springbootregister/config/Init.java
+++ src/main/java/com/codecool/springbootregister/config/Init.java
@@ -52,9 +52,45 @@ public class Init implements CommandLineRunner {
                 .car(myCar)
                 .hireDate(LocalDate.now())
                 .construction("Béla utca 23")
-                .name("Meszaros Lorinc").build();
+                .name("István Imre").build();
 
         employeeRepository.save(employee);
+
+        Employee employee4 = Employee.builder()
+                .address("Ló utca 2")
+                .availability(AvailabilityType.AVAILABLE)
+                .hireDate(LocalDate.now())
+                .construction("Béla utca 23")
+                .name("István Péter").build();
+        employeeRepository.save(employee4);
+
+        Employee employee1 = Employee.builder()
+                .address("Ló utca 2")
+                .availability(AvailabilityType.NOTAVAILABLE)
+
+                .hireDate(LocalDate.now())
+                .construction("Béla utca 23")
+                .name("Imre Ádám").build();
+
+        employeeRepository.save(employee1);
+
+        Employee employee2 = Employee.builder()
+                .address("Pillangó utca 2")
+                .availability(AvailabilityType.SICKLEAVE)
+                .hireDate(LocalDate.now())
+                .construction("Ó utca 1")
+                .name("Jó Pál").build();
+
+        employeeRepository.save(employee2);
+
+        Employee employee3 = Employee.builder()
+                .address("Pillangó utca 2")
+                .availability(AvailabilityType.HOLIDAY)
+                .hireDate(LocalDate.now())
+                .construction("Ó utca 1")
+                .name("Kovács Zsolt").build();
+
+        employeeRepository.save(employee3);
 
         Salary salary = Salary.builder()
                 .amount(new BigDecimal(1000))
@@ -64,22 +100,111 @@ public class Init implements CommandLineRunner {
                 .build();
 
         Salary salary2 = Salary.builder()
-                .amount(new BigDecimal(500))
-                .type(SalaryType.NORMAL)
+                .amount(new BigDecimal(50))
+                .type(SalaryType.INADVANCE)
                 .employee(employee)
-                .paymentDate(LocalDate.of(2000,2,4))
+                .paymentDate(LocalDate.of(2001,2,4))
                 .build();
 
         Salary salary3 = Salary.builder()
+                .amount(new BigDecimal(1000))
+                .type(SalaryType.NORMAL)
+                .employee(employee)
+                .paymentDate(LocalDate.of(2002,2,4))
+                .build();
+
+        Salary salary4 = Salary.builder()
+                .amount(new BigDecimal(50))
+                .type(SalaryType.INADVANCE)
+                .employee(employee)
+                .paymentDate(LocalDate.of(2003,2,4))
+                .build();
+
+        Salary salary5 = Salary.builder()
+                .amount(new BigDecimal(1000))
+                .type(SalaryType.NORMAL)
+                .employee(employee)
+                .paymentDate(LocalDate.of(2004,2,4))
+                .build();
+
+        Salary salary6 = Salary.builder()
+                .amount(new BigDecimal(50))
+                .type(SalaryType.INADVANCE)
+                .employee(employee)
+                .paymentDate(LocalDate.of(2005,2,4))
+                .build();
+
+        Salary salary7 = Salary.builder()
+                .amount(new BigDecimal(1000))
+                .type(SalaryType.NORMAL)
+                .employee(employee)
+                .paymentDate(LocalDate.of(2006,2,4))
+                .build();
+
+        Salary salary8 = Salary.builder()
                 .amount(new BigDecimal(500))
                 .type(SalaryType.NORMAL)
                 .employee(employee)
-                .paymentDate(LocalDate.of(2000,2,2))
+                .paymentDate(LocalDate.of(2007,2,4))
+                .build();
+
+        Salary salary9 = Salary.builder()
+                .amount(new BigDecimal(500))
+                .type(SalaryType.NORMAL)
+                .employee(employee)
+                .paymentDate(LocalDate.of(2008,2,4))
+                .build();
+
+        Salary salary10 = Salary.builder()
+                .amount(new BigDecimal(500))
+                .type(SalaryType.NORMAL)
+                .employee(employee)
+                .paymentDate(LocalDate.of(2009,2,4))
+                .build();
+
+
+        Salary salary11 = Salary.builder()
+                .amount(new BigDecimal(50))
+                .type(SalaryType.INADVANCE)
+                .employee(employee)
+                .paymentDate(LocalDate.of(2010,2,2))
+                .build();
+
+        Salary salary12 = Salary.builder()
+                .amount(new BigDecimal(500))
+                .type(SalaryType.NORMAL)
+                .employee(employee)
+                .paymentDate(LocalDate.of(2011,2,2))
+                .build();
+
+        Salary salary13 = Salary.builder()
+                .amount(new BigDecimal(50))
+                .type(SalaryType.INADVANCE)
+                .employee(employee)
+                .paymentDate(LocalDate.of(2012,2,2))
+                .build();
+
+        Salary salary14 = Salary.builder()
+                .amount(new BigDecimal(500))
+                .type(SalaryType.NORMAL)
+                .employee(employee)
+                .paymentDate(LocalDate.of(2013,2,2))
                 .build();
 
         salaryRepository.save(salary);
         salaryRepository.save(salary2);
         salaryRepository.save(salary3);
+        salaryRepository.save(salary4);
+        salaryRepository.save(salary5);
+        salaryRepository.save(salary6);
+        salaryRepository.save(salary7);
+        salaryRepository.save(salary8);
+        salaryRepository.save(salary9);
+        salaryRepository.save(salary10);
+        salaryRepository.save(salary11);
+        salaryRepository.save(salary12);
+        salaryRepository.save(salary13);
+        salaryRepository.save(salary14);
 
         User user = new User("admin", encoder.encode("admin"), roles);
 
